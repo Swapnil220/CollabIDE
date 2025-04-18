147239b9ae61f42703187d1824517ad9e7e1462a
Swapnil2524 <swapnil.pawar@utopiatech.in>
2025-04-18 10:43:33 +0530

    test routing problem

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,9 +8,9 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <AuthProvider>
-            {/* <BrowserRouter> */}
+            <BrowserRouter>
                 <App />
-            {/* </BrowserRouter> */}
+            </BrowserRouter>
         </AuthProvider>
      </React.StrictMode>
 );
